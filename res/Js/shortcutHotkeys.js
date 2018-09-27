b2d7ebfa5be74f1e0a35606466cbaa9a05f07d8f
danjake102 <danjakeramos@gmail.com>
2018-09-27 13:30:55 -0700

    Key press test

--- res/Js/shortcutHotkeys.js
+++ res/Js/shortcutHotkeys.js
@@ -1,1 +1,9 @@
-console.log("test");
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: console.log("test");
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
