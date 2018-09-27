7109fa379187eebaa5aafeae31a837a9f7a8bcfb
danjake102 <danjakeramos@gmail.com>
2018-09-27 12:53:20 -0700

    Update shortcutHotkeys.js

--- res/Js/shortcutHotkeys.js
+++ res/Js/shortcutHotkeys.js
@@ -1,13 +1,1 @@
-$(function () {
-var isCtrl=false
-var isAlt =false
-$(document).bind('keydown',function(e) {
-	if (e.ctrlKey && e.which == 66) {
-    	alert("Ctrl + B shortcut combination was pressed");
-    }
-});
-$(document).bind('keyup',function(e) {
-				isCtrl = false
-				isAlt = false
-});
-});
+console.log("test");
