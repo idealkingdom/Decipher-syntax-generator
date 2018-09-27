7a40e285d71ba539731becda1a9a123e469329af
danjake102 <danjakeramos@gmail.com>
2018-09-27 13:47:03 -0700

    Update shortcutHotkeys.js

--- res/Js/shortcutHotkeys.js
+++ res/Js/shortcutHotkeys.js
@@ -1,7 +1,8 @@
 function doc_keyUp(e) {
 
     if (e.ctrlKey && e.keyCode == 49) {
-        elementType('row')
+        elementType('row');
+        console.log("Loaded");
     }
 }
 document.addEventListener('keyup', doc_keyUp, false);
