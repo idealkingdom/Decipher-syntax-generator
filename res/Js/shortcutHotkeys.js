940bfaa7dcf8b1e7fe9fe509773c51ef831876b2
danjake102 <danjakeramos@gmail.com>
2018-09-27 13:53:11 -0700

    Testing

--- res/Js/shortcutHotkeys.js
+++ res/Js/shortcutHotkeys.js
@@ -1,13 +1,9 @@
+console.log("Up and running");
+
 function doc_keyUp(e) {
 
-<<<<<<< HEAD
     if (e.ctrlKey && e.altKey && e.keyCode == 49) {
-        elementType('row')
-=======
-    if (e.ctrlKey && e.keyCode == 49) {
         elementType('row');
-        console.log("Loaded");
->>>>>>> 7a40e285d71ba539731becda1a9a123e469329af
     }
 }
 document.addEventListener('keyup', doc_keyUp, false);
