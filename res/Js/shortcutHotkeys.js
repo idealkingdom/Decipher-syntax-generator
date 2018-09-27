10592575ed0c5f30ddcf8255325b1ae2a63a96c0
danjake102 <danjakeramos@gmail.com>
2018-09-27 14:01:34 -0700

    Added shortcut key for adding row and radio

--- res/Js/shortcutHotkeys.js
+++ res/Js/shortcutHotkeys.js
@@ -2,8 +2,18 @@ console.log("Up and running");
 
 function doc_keyUp(e) {
 
-    if (e.ctrlKey && e.altKey && e.keyCode == 49) {
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 49) {
+    	//CTRL + SHIFT + 1
         elementType('row');
     }
+
+    else if (e.ctrlKey && e.shiftKey && e.keyCode == 82) {
+    	//CTRL + SHIFT + R
+        questionType('radio');
+    }
+
+
+
+
 }
 document.addEventListener('keyup', doc_keyUp, false);
