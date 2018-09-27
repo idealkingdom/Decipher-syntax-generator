65bc3bd02347a5ce986a255001219cd99e7f3d39
danjake102 <danjakeramos@gmail.com>
2018-09-27 14:21:32 -0700

    updated keyboard shortcuts

--- res/Js/shortcutHotkeys.js
+++ res/Js/shortcutHotkeys.js
@@ -3,18 +3,15 @@ console.log("Again22");
 function doc_keyUp(e) {
 
     if (e.ctrlKey && e.keyCode == 49) {
-    	//CTRL + SHIFT + 1
+    	//CTRL + ALT + 1
         elementType('row');
     }
 
     else if (e.ctrlKey && e.keyCode == 82) {
-    	//CTRL + R
+    	//CTRL + ALT + R
         questionType('radio');
         event.preventDefault();
     }
-
-
-
 
 }
 document.addEventListener('keyup', doc_keyUp, false);
